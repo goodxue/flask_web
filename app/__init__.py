a04ad0ae5ef8bbfaa09e9dc3ef2027a58cfd0eef
goodxue <627807499@qq.com>
2019-05-19 11:57:04 +0800

    Architecture

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,14 @@
-from Flask import Flask
+'''
+@Time    :   2019/05/19 11:32:14
+'''
+__AUTHOR__ = 'xwp' 
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run() 
